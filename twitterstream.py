a902e6bef499fedd43143fbc83cb3394b3638d40
Saggi Mizrahi <ficoos@gmail.com>
2018-11-27 11:08:08 +0200

    Allow passing secret information in the environment
    Putting the secrets in the sources is not very smart and makes the code less maintainable.
    Using the cmdline is also not very smart as the secrets will be visible when listing the processes.
    Passing the information in the environment is a secure and easy way to pass this information.

--- twitterstream.py
+++ twitterstream.py
@@ -3,15 +3,16 @@ import datetime
 import json
 import time
 import uuid
+import os
 
 import bottle
 from feedgen import feed
 import twitter
 
-api_key = ""
-api_secret = ""
-access_token_key = ""
-access_token_secret = ""
+api_key = os.environ.get("TIS_API_KEY", "")
+api_secret = os.environ.get("TIS_SECRET", "")
+access_token_key = os.environ.get("TIS_ACCESS_TOKEN_KEY", "")
+access_token_secret = os.environ.get("TIS_ACCESS_TOKEN_SECRET", "")
 
 uuid_namespace = uuid.UUID('e8cd8531-a8df-4b41-a249-479d15252349')
 
